53338a825b76f85f85794227d1c4ca9e3a8ea69a
DeniseGranode <grandoedenise@gmail.com>
2023-03-22 20:58:08 -0700

    new comit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
+
